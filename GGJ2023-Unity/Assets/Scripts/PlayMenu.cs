b34e43c5faf19ada1f01082b4a1a7b6047aa2ea4
Rúben Miguel Oliveira Tadeia <rubent03@gmail.com>
2023-02-05 12:14:55 +0000

    Level 2 and 3

--- GGJ2023-Unity/Assets/Scripts/PlayMenu.cs
+++ GGJ2023-Unity/Assets/Scripts/PlayMenu.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class PlayMenu : MonoBehaviour
 {
+
     public void RestartGame (){
+		SwitchPlay.play = false;
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 	public void BackToMenu (){
