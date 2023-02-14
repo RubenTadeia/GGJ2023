11b8b6bcbfd5b30f7a87fc865dd9b9fdd9a6bac4
Rúben Miguel Oliveira Tadeia <rubent03@gmail.com>
2023-02-14 16:17:49 +0000

    Fixed Sound Issue for all levels and sound controller

--- GGJ2023-Unity/Assets/Scripts/VolumeController.cs
+++ GGJ2023-Unity/Assets/Scripts/VolumeController.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class VolumeController : MonoBehaviour
 {
-	public Slider volumeSlider;
-	public AudioSource audioSource;
-
-	private void Start()
-	{
-		volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
-	}
-
-	private void OnVolumeChanged(float value)
-	{
-		audioSource.volume = value;
+	public AudioMixer mixer;
+	
+	public void SetLevel (float sliderValue){
+		
+		mixer.SetFloat("Music",Mathf.Log10(sliderValue) * 20);
+		
 	}
 }
